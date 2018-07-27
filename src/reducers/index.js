3ddac17aef96f4e5da5263407fe75563d6a57a39
Ajay <ajay1kumar1@gmail.com>
2018-07-27 08:17:20 +0530

    redux

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from '../../../../../../Users/Leena/AppData/Local/Microsoft/TypeScript/2.9/node_modules/redux';
+import postReducer from './postReducer';
+
+export default combineReducers({
+    posts:postReducer
+});
